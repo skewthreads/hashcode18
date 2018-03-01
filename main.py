a4264879736a00dd8166bd925a9f5b7735f58f5c
Thanos Giannopoulos <thanosgn@gmail.com>
2018-03-01 21:49:06 +0200

    First approach, added inputs

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ vehicle_dict = {}
 vehicle_list = []
 free_vehicles = {}
 all_rides_assigned = False
+time_dict = {} # when its time to update a vehicle
 
 class Ride:
     def __init__(self, id, start_x, start_y, end_x, end_y, earliest_start, latest_finish):
@@ -33,14 +34,7 @@ class Vehicle:
         self.current_y = 0
         self.assigned_rides = [] # index of assigned rides
         self.free = True
-    for current_time in range(simulation_time):
-        for vehicle in vehicle_list:
-            if vehicle.free == False:
-                continue
-            for ride in ride_list:
-                if ride.done == True:
-                    continue
-                score = score(vehicle, ride)
+
 
 
 def manhattan(start_x, start_y, end_x, end_y):
@@ -50,12 +44,14 @@ def score(vehicle, ride):
     score = 0
     distance_to_starting_point = manhattan(vehicle.current_x, vehicle.current_y, ride.start_x, ride.start_y)
     ride_distance = ride.distance
-    starting_time = current_time + distance_to_starting_point
-    if starting_time < ride.earliest_start:
+    starting_time = max(current_time + distance_to_starting_point, ride.earliest_start)
+    # starting_time = current_time + distance_to_starting_point
+    total_time = starting_time + ride_distance
+    if starting_time <= ride.earliest_start:
         score += per_ride_bonus
-    if starting_time + ride_distance < ride.latest_finish:
+    if total_time < ride.latest_finish:
         score += ride.distance
-    return score # maybe subtract distance_to_starting_point*some_factor
+    return score / total_time # maybe subtract distance_to_starting_point*some_factor
 
 
 
@@ -75,7 +71,8 @@ def main():
     global vehicle_dict
     global free_vehicles
     global all_rides_assigned
-    with open('inputs/a_example.in', 'r') as inputFile:
+    with open('/home/thanos/repositories/louis/inputs/a_example.in', 'r') as inputFile:
+    # with open('/home/thanos/repositories/louis/inputs/b_should_be_easy.in', 'r') as inputFile:
         header = next(inputFile).split()
         lines = int(header[0])
         columns = int(header[1])
@@ -94,6 +91,12 @@ def main():
         free_vehicles[v] = True
 
     for current_time in range(simulation_time):
+        if current_time in time_dict: # if its time to update a vehicle
+            for vehicle in time_dict[current_time]:
+                vehicle.free = True
+                last_ride = vehicle.assigned_rides[-1]
+                vehicle.current_x = last_ride.end_x
+                vehicle.current_y = last_ride.end_y
         if all_rides_assigned:
             break
         for vehicle in vehicle_list:
@@ -109,9 +112,25 @@ def main():
                 break
             best_ride = max(scores, key=lambda x: x[1])[0]
             best_ride.free = False
-            vehicle.assigned_rides.append(best_ride.id)
+            vehicle.assigned_rides.append(best_ride)
             vehicle.free = False
+            distance_to_starting_point = manhattan(vehicle.current_x, vehicle.current_y, ride.start_x, ride.start_y)
+            ending_time = current_time + distance_to_starting_point + ride.distance
+            if ending_time in time_dict:
+                time_dict[ending_time].append(vehicle)
+            else:
+                time_dict[ending_time] = [vehicle]
+
+def output():
+    with open('outputs/a_example.out', 'w') as outputFile:
+    # with open('outputs/b_example.out', 'w') as outputFile:
+        for vehicle in vehicle_list:
+            rides = ''
+            for ride in vehicle.assigned_rides:
+                rides += ' ' + str(ride.id)
+            outputFile.write(str(len(vehicle.assigned_rides)) + rides + '\n' )
 
 
 if __name__ == '__main__':
     main()
+    output()
